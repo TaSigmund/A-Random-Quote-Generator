60edd364f2ea64fb8816b07279f9e5609afadb11
TaSigmund <64551452+TaSigmund@users.noreply.github.com>
2020-05-05 21:42:24 +0200

    added quotes and getRandomQuote()
    I added an array of objects containing a small selection of quotes. I also added a function that returns a random quote.

--- js/script.js
+++ js/script.js
@@ -11,12 +11,55 @@ project 1 - A Random Quote Generator
  * `quotes` array 
 ***/
 
+let quotes = [
+  {
+      quote: 'Education is the most powerful weapon which you can use to change the world.',
+      source: 'Nelson Mandela',
+      citation: 'Speech, Madison Park High School, Boston',
+      year: 1990
+  },
+  {
+      quote: 'If you want to know the reason why I\'m standing here, it\'s because of education. I never cut class.',
+      source: 'Michelle Obama',
+      citation: 'Elizabeth Garrett Anderson School, London',
+      year: 2009
+  },
+  {
+      quote: 'Nothing in all the world is more dangerous than sincere ignorance and conscientious stupidity.',
+      source: 'Martin Luther King',
+      citation: 'Strength to Love, ch. 4',
+      year: 1963
+  },
+  {
+      quote: 'Technology is just a tool. In terms of getting the kids working together and motivating them, the teacher is the most important.',
+      source: 'Bill Gates',
+      citation: 'Independent on Sunday',
+      year: 1997
+  },
+  {
+      quote: 'No pessimist ever discovered the secret of the stars or sailed to an uncharted land.',
+      source: 'Helen Keller',
+      citation: 'Optimism: an essay. Pt. 3',
+      year: 1903
+  },
+  {
+      quote: 'One child, one teacher, one book and one pen can change the world. Education is the only solution. Education first.',
+      source: 'Malala Yousafzai',
+      citation: 'United Nations General Assembly',
+      year: 2013
+  }
+]
 
 
 /***
  * `getRandomQuote` function
 ***/
 
+function getRandomQuote() {
+
+  let randomNumber = Math.floor(Math.random() * ((quotes.length -1) - Math.ceil(0) + 1)) + Math.ceil(0); // random number between 0 and lenght of the array - 1. Based on a code snippet on: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+  return quotes[randomNumber]
+}
 
 
 /***
